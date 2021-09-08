4c8297f2e150f7b6fd9421fdc0d3551b6131b344
ben fleis <benfleis@uber.com>
2021-09-08 09:10:25 +0200

    checkin clj-new templated project before code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to find-dups
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/benfleis/find_dups_test.clj
@@ -0,0 +1,7 @@
+(ns benfleis.find-dups-test
+  (:require [clojure.test :refer :all]
+            [benfleis.find-dups :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
